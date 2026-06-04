4dbcc81f18e5a8294795486eb2fcad64e2438ed7
kuaruan <kuaruan.projects@gmail.com>
2026-06-04 19:05:54 -0400

    Actualización inicial test_health.py
    Agregando librerías, registro de logs

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,11 +1,27 @@
+import logging
+import os
+import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [AUDIT_LOG] [%(levelname)s]: %(message)s",
+    handlers = [
+        logging.FileHandler("app.log", mode="w", encoding="utf-8"), # Limpieza log antiguo
+        logging.StreamHandler() # Logs en vivo
+    ]
+)
+
 client = TestClient(app)
 
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
+def test_api():
+    logging.info("Verificando disponibilidad de api")
+    try:
+        
+        response = client.get("/")
+        
+        assert response.status_code == 200
 
 def test_health():
     response = client.get("/health")
